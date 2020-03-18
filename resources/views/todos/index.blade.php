432ccaf4e9bcca9460afd36af54c486e8c704177
imtiajinfo <imtiaj.info@gmail.com>
2020-03-18 18:58:08 +0600

    first commit

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('title')
+	Todos List 
+@endsection
+@section('content')
+ 
+		<h1 class="text-center my-5">Todos page</h1>
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card card-default">
+					<div class="card-header">
+						Todos
+					</div>
+					<div class="card-body">
+						<ul class="list-group">
+							@foreach($todos as $todo)
+								<li class="list-group-item">
+									{{ $todo->name }}
+								<a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
+                @if(!$todo->completed)
+                  <a href="/todos/{{ $todo->id }}/complete" style="color: white;" class="btn btn-warning btn-sm float-right">Complete</a>
+                @endif	
+						
+								</li>
+							@endforeach
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+@endsection		
